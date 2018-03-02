38b2d70c3cc58ee36cfc6f80e8c9d646d05e84cf
Victor Borja <vborja@apache.org>
2018-03-02 17:14:16 -0600

    package

--- mix.exs
+++ mix.exs
@@ -7,7 +7,8 @@ defmodule MixUnder.MixProject do
       version: "0.1.0",
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      package: package()
     ]
   end
 
@@ -21,8 +22,17 @@ defmodule MixUnder.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false},
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
+    ]
+  end
+
+  defp package do
+    [
+      maintainers: ["Victor Borja <vborja@apache.org>"],
+      licenses: ["Apache-2"],
+      links: %{"GitHub" => "https://github.com/vic/mix_under"}
     ]
   end
 end
